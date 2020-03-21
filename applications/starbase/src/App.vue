f1817f29cef8807eea36a65b04d5a121d99e3b0c
Jonathan Robson <jonrob08@gmail.com>
2020-03-21 10:19:06 -0400

    Added Team Generation functionality
    Added a Character component and imported it into the project. The components data function returns an empty object and it also has the fetch method from the app and a switch character method that randomizes the fetch URL character number. Added created function so it fired upon creation. Inside the project there is now an initial ids array and is binded to the character via the props array added on the character component. Should randomize ids within cards. Added switch character to cards on click. Styled cards a bit.

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,26 +1,30 @@
 <template>
 <div id="app">
-    <h3>{{title}}</h3>
-    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
+    <h3>{{title}}</h3>    
+    <div class="col-md-12">
+        <Character 
+            v-for="(id, index) in initial_ids"
+            :id="id"
+            key="index"
+        />
+    </div>
 </div>
 </template>
 
 <script>
+
+import Character from './components/Character'
+
 export default {
     name: 'app',
     data() {
         return {
-            title: 'Generate Your Star Base Team'
+            title: 'Generate Your Star Base Team',
+            initial_ids: [1, 13, 14]
         }
     },
-    methods: {
-        fetchCharacter() {
-            fetch('https://swapi.co/api/people/', {
-                method: 'GET'
-            })
-                .then(response => response.json())
-                .then(json => console.log(json))                       
-        }
+    components: {
+        Character
     }
 }
 </script>
